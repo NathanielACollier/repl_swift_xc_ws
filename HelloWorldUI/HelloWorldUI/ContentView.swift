10dc08f39f40c0cbbb3f2a3d3b0dc3825f5f0bcf
Nathaniel Collier <drxs3v3n@gmail.com>
2023-12-29 11:17:56 -0600

    Start a simple swiftui hello world

---
+++ HelloWorldUI/HelloWorldUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorldUI
+//
+//  Created by Nathaniel Collier on 12/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWorldUI/HelloWorldUI/HelloWorldUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldUIApp.swift
+//  HelloWorldUI
+//
+//  Created by Nathaniel Collier on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
